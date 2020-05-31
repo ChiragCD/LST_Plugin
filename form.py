24448a44d16e66672724b8f6bf778902c04226da
Nilesh Kumar Gupta <f20180233@goa.bits-pilani.ac.in>
2020-05-31 13:20:09 +0530

    To save the outputs

--- form.py
+++ form.py
@@ -1,14 +1,21 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+from qgis.PyQt.QtGui import *
+from qgis.PyQt.QtWidgets import *
+from qgis.PyQt.QtCore import *
 from PIL import Image
+from zipfile import ZipFile
 import numpy
+
+from . import procedures
+
+# import procedures
+import os
 
 fileNames = set()
 filePaths = set()
 resultStates = []
 checkboxes = []
 band = [0, 0, 0]
+radios = []
 
 
 class MainWindow(QMainWindow):
@@ -20,7 +27,7 @@ class MainWindow(QMainWindow):
 
         # input file option
         label = QLabel()
-        label.setText("Input file")
+        label.setText("Input bands")
         label.setAlignment(Qt.AlignCenter)
         layout.addWidget(label)
 
@@ -28,20 +35,52 @@ class MainWindow(QMainWindow):
         for i in (4, 5, 10):
             layout.addWidget(self.browseFile(i))
 
-        # checkbox for various outputs
+        # select data type
+        dtwidget = QWidget()
+        hlayout = QHBoxLayout()
 
+        label = QLabel("Select Data type")
+        hlayout.addWidget(label)
+
+        lst5button = QRadioButton("Landsat5")
+        radios.append(lst5button)
+        hlayout.addWidget(lst5button)
+
+        lst8button = QRadioButton("Landsat8")
+        lst8button.setChecked(True)
+        radios.append(lst8button)
+        hlayout.addWidget(lst8button)
+
+        dtwidget.setLayout(hlayout)
+        layout.addWidget(dtwidget)
+
+        # select output types lable
+        label = QLabel()
+        label.setText("Select Output types")
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+
+        # checkbox for various outputs
         toacheckbox = QCheckBox("TOA Spectral Radiance")
         layout.addWidget(toacheckbox)
         checkboxes.append(toacheckbox)
+
+        btcheckbox = QCheckBox("At Sensor Brightness Temperature")
+        layout.addWidget(btcheckbox)
+        checkboxes.append(btcheckbox)
+
         ndvicheckbox = QCheckBox("NDVI")
         layout.addWidget(ndvicheckbox)
         checkboxes.append(ndvicheckbox)
-        vegcheckbox = QCheckBox("Proportion of Vegetation")
-        layout.addWidget(vegcheckbox)
-        checkboxes.append(vegcheckbox)
+
+        pvcheckbox = QCheckBox("Proportion of Vegetation")
+        layout.addWidget(pvcheckbox)
+        checkboxes.append(pvcheckbox)
+
         lsecheckbox = QCheckBox("Land Surface Emissivity")
         layout.addWidget(lsecheckbox)
         checkboxes.append(lsecheckbox)
+
         lstcheckbox = QCheckBox("LST")
         lstcheckbox.setChecked(True)
         layout.addWidget(lstcheckbox)
@@ -74,29 +113,56 @@ class MainWindow(QMainWindow):
 
     def getFiles(self, pathField, num):
         fp = QFileDialog.getOpenFileName()
-        print(fp[0])
+        # print(fp[0])
         pathField.setText(fp[0])
         if not fileNames.__contains__(fp[0]):
             fileNames.add(fp[0])
             filePaths.add((fp[0], num))
 
     def goFunc(self):
+
         if len(fileNames) != 3:
-            print("All files not specified")
-            # show an error
+            print("An error occured")
             return
 
         for box in checkboxes:
             resultStates.append(box.isChecked())
 
-        loadBands(filePaths)
-        # show a loader
-        #
-        #
-        #
-        #
-        # run all the functions here
+        satType = radios[0].text() if radios[0].isChecked() else radios[1].text()
 
+        loadBands(filePaths)
+
+        toa, bt, ndvi, pv, lse, lst = procedures.process(
+            band[0], band[1], band[2], satType
+        )
+
+        # os.mkdir("LSTPluginResults")
+        os.makedirs("~/LSTPluginResults")
+        print(resultStates[5])
+
+        # zipObj = ZipFile.open("results.zip", "w")
+
+        if resultStates[0]:
+            toaIm = Image.fromarray(toa)
+            toaIm.save("~/LSTPluginResults/TOA.tiff")
+        if resultStates[1]:
+            btIm = Image.fromarray(bt)
+            btIm.save("~/LSTPluginResults/BT.tiff")
+        if resultStates[2]:
+            ndviIm = Image.fromarray(ndvi)
+            ndviIm.save("~/LSTPluginResults/NDVI.tiff")
+        if resultStates[3]:
+            pvIm = Image.fromarray(pv)
+            pvIm.save("~/LSTPluginResults/PV.tiff")
+        if resultStates[4]:
+            lseIm = Image.fromarray(lse)
+            lseIm.save("~/LSTPluginResults/LSE.tiff")
+        if resultStates[5]:
+            lstIm = Image.fromarray(lst)
+            lstIm.save("~/LSTPluginResults/LST.tiff")
+        print("All done")
+
+        self.close()
         # output the data as a file
 
 
@@ -104,4 +170,3 @@ def loadBands(fp):
     for path in fp:
         im = Image.open(path[0])
         band[path[1] % 4] = numpy.array(im)
-    print(band)
--- procedures.py
+++ procedures.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def calc_TOA_radiance(tir, sat_type):
 
     """
@@ -10,10 +11,16 @@ def calc_TOA_radiance(tir, sat_type):
         toa - numpy array
     """
 
-    data = {"Landsat8" : {"mul" : 0.0003342, "add" : -0.19}, ##-0.19 = 0.1 - 0.29 (landsat 8 band 10 correction)
-            "Landsat5" : {"mul" : 0.055375, "add" : 1.18243}}
+    data = {
+        "Landsat8": {
+            "mul": 0.0003342,
+            "add": -0.19,
+        },  ##-0.19 = 0.1 - 0.29 (landsat 8 band 10 correction)
+        "Landsat5": {"mul": 0.055375, "add": 1.18243},
+    }
     toa = (tir * data[sat_type]["mul"]) + data[sat_type]["add"]
     return toa
+
 
 def calc_BT(toa, sat_type):
 
@@ -25,10 +32,13 @@ def calc_BT(toa, sat_type):
         bt - numpy array
     """
 
-    data = {"Landsat8" : {"K1" : 1321.0789, "K2" : 774.8853},
-            "Landsat5" : {"K1" : 607.76, "K2" : 1260.56}}
+    data = {
+        "Landsat8": {"K1": 1321.0789, "K2": 774.8853},
+        "Landsat5": {"K1": 607.76, "K2": 1260.56},
+    }
     bt = (data[sat_type]["K2"] / np.log((data[sat_type]["K1"] / toa) + 1)) - 273.15
     return bt
+
 
 def calc_NDVI(nir, r):
 
@@ -43,6 +53,7 @@ def calc_NDVI(nir, r):
     ndvi = (nir - r) / (nir + r)
     return ndvi
 
+
 def calc_PV(ndvi):
 
     """
@@ -53,9 +64,12 @@ def calc_PV(ndvi):
         pv - numpy array
     """
 
-    data = {"ndvi_soil" = 0.2, "ndvi_vegetation" = 0.5}
-    pv = np.square((ndvi - data["ndvi_soil"]) / (data["ndvi_vegetation"] - data["ndvi_soil"]))
+    data = {"ndvi_soil": 0.2, "ndvi_vegetation": 0.5}
+    pv = np.square(
+        (ndvi - data["ndvi_soil"]) / (data["ndvi_vegetation"] - data["ndvi_soil"])
+    )
     return pv
+
 
 def calc_LSE(ndvi, pv):
 
@@ -67,12 +81,19 @@ def calc_LSE(ndvi, pv):
         lse - numpy array
     """
 
-    data = {"water_emissivity" : 0.991, "soil_emissivity" : 0.996, "vegetation_emissivity" : 0.973}
+    data = {
+        "water_emissivity": 0.991,
+        "soil_emissivity": 0.996,
+        "vegetation_emissivity": 0.973,
+    }
     lse = np.full(ndvi.shape, data["water_emissivity"])
     lse[ndvi > 0] = data["soil_emissivity"]
-    lse[ndvi > 0.2] += (pv[ndvi > 0.2] * (data["vegetation_emissivity"] - data["soil_emissivity"]))
+    lse[ndvi > 0.2] += pv[ndvi > 0.2] * (
+        data["vegetation_emissivity"] - data["soil_emissivity"]
+    )
     lse[ndvi > 0.5] = data["vegetation_emissivity"]
     return lse
+
 
 def calc_LST(bt, lse):
 
@@ -84,9 +105,10 @@ def calc_LST(bt, lse):
         lst - numpy array
     """
 
-    data = ["lambda" : 0.00115, "rho" : 1.4388] ##Verify values, only ratio important
+    data = {"lambda": 0.00115, "rho": 1.4388}  ##Verify values, only ratio important
     lst = bt / (1 + (data["lambda"] * bt / data["rho"]) * np.log(lse))
     return lst
+
 
 def process(r, nir, tir, sat_type):
 
@@ -102,7 +124,7 @@ def process(r, nir, tir, sat_type):
     toa = calc_TOA_radiance(tir, sat_type)
     bt = calc_BT(toa, sat_type)
     ndvi = calc_NDVI(nir, r)
-    pv = calc_pv(ndvi)
+    pv = calc_PV(ndvi)
     lse = calc_LSE(ndvi, pv)
     lst = calc_LST(bt, lse)
     return toa, bt, ndvi, pv, lse, lst
